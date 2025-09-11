8dfcf1d3f4b63636a7045af142c2b3f037b6e7a5
Dora Do <doradocodes@gmail.com>
2025-09-10 23:35:21 -0400

    init

---
+++ script.js
@@ -0,0 +1,58 @@
+// ADD YOUR CODE BELOW 
+
+// 1. Start with an array of strings (ex: "grapes", "bread", "tea")
+let items = [
+
+];
+
+
+
+
+// 2. Create variables for each interactive DOM element
+const addItemButton = document.getElementById('add-item-button');
+// add more variables 
+
+
+
+
+// 3. Write a function to render all items onto the page
+function updateList() {
+    // add your code here
+}
+
+
+
+
+// 4. Handle adding a new item when the form is submitted
+addItemButton.addEventListener('click', function () {
+    // add your code here
+});
+
+
+
+
+// 5. Sort items alphabetically when sortBtn is clicked
+sortBtn.addEventListener("click", () => {
+    // add your code here
+});
+
+
+
+
+// 6. Transform items to uppercase when upperBtn is clicked
+upperBtn.addEventListener("click", () => {
+    // add your code here
+});
+
+
+
+
+// 7. Clear all items when clearBtn is clicked
+clearBtn.addEventListener("click", () => {
+    // add your code here
+});
+
+
+
+
+displayList();
