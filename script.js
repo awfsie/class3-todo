1381fdae8935f8427eebcfda0382fc392e648a3b
Dora Do <doradocodes@gmail.com>
2025-09-18 12:15:04 -0400

    Revert "Added solution to HW3"
    This reverts commit bf1e78bfe5ab1f163960308a01e3b340eb64afa1.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // 1. Start with an array of strings (ex: "grapes", "bread", "tea")
 let todoItems = [
     // add more items here
-    'grapes', 'bread', 'tea', 'milk'
 ];
 
 
@@ -12,42 +11,21 @@ let todoItems = [
 const addItemButton = document.getElementById('add-item-button');
 // add more variables below
 const list = document.getElementById('list');
-const textInput = document.getElementById('text');
-const sortBtn = document.getElementById('sort');
-const clearBtn = document.getElementById('clear');
 
 
 
 // 3. Write a function to display all items in the #list element
 function updateList() {
-    list.innerHTML = '';
-
-    // go through each item in todoItems
-    for (let i = 0; i < todoItems.length; i++) {
-        addItem(todoItems[i]);
-    }
+    // add your code here
 }
 
 updateList();
-
-function addItem(newItem) {
-    // create a <li> tag for each item
-    const listItem = document.createElement('li');
-    listItem.innerText = newItem;
-
-    // add that <li> to the <ul id="list">
-    list.appendChild(listItem);
-}
 
 
 
 // 4. Handle adding a new item when the form is submitted
 addItemButton.addEventListener('click', function () {
-    const newItem = textInput.value;
-    todoItems.push(newItem);
-    updateList();
-
-    textInput.value = '';
+    // add your code here
 });
 
 
@@ -55,8 +33,7 @@ addItemButton.addEventListener('click', function () {
 
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
-    todoItems.sort();
-    updateList();
+    // add your code here
 });
 
 
@@ -64,7 +41,5 @@ sortBtn.addEventListener("click", () => {
 
 // 6. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
-    // list.innerHTML = '';
-    todoItems = [];
-    updateList();
+    // add your code here
 });
